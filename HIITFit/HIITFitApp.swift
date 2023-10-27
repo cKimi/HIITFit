c96f2facc198630e2715f72ca7dad956abb14e0d
Carlos Kimura <ckimura@gmail.com>
2023-10-27 10:49:11 -0300

    initial project files

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Carlos Kimura on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
