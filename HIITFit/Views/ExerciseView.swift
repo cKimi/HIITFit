e291e0e90a8b55c23e1e6ca4f2453e52e00a7d8f
Carlos Kimura <ckimura@gmail.com>
2023-11-01 16:07:44 -0300

    set bundle url to play exercise videos

--- HIITFit/Views/ExerciseView.swift
+++ HIITFit/Views/ExerciseView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import AVKit
 
 struct ExerciseView: View {
     
@@ -15,13 +16,22 @@ struct ExerciseView: View {
     }
     
     var body: some View {
-        VStack {
-            HeaderView(exerciseName: exercise.exerciseName)
-            Text("Video player")
-            Text("Timer")
-            Text("Start/Done button")
-            Text("Rating")
-            Text("History Button")
+        GeometryReader { geometry in
+            VStack {
+                HeaderView(exerciseName: exercise.exerciseName)
+                    .padding(.bottom)
+                if let url = Bundle.main.url(forResource: exercise.videoName, withExtension: ".mp4") {
+                    VideoPlayer(player: AVPlayer(url: url))
+                        .frame(height: geometry.size.height * 0.45)
+                } else {
+                    Text("Couldn't not find \(exercise.videoName).mp4")
+                        .foregroundColor(.red)
+                }
+                Text("Timer")
+                Text("Start/Done button")
+                Text("Rating")
+                Text("History Button")
+            }
         }
     }
 }
