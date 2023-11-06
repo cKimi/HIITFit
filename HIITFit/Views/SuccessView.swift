dbbc576b9b78c1198df67cd2302f6086a67582f8
Carlos Kimura <ckimura@gmail.com>
2023-11-06 16:29:55 -0300

    success view created

---
+++ HIITFit/Views/SuccessView.swift
@@ -0,0 +1,42 @@
+//
+//  SuccessView.swift
+//  HIITFit
+//
+//  Created by Carlos Kimura on 06/11/23.
+//
+
+import SwiftUI
+
+struct SuccessView: View {
+    var body: some View {
+        ZStack {
+            VStack {
+                Image(systemName: "hand.raised.fill")
+                    .resizedToFill(width: 75.0, height: 75.0)
+                    .foregroundColor(.purple)
+                
+                Text("High Five!")
+                    .font(.largeTitle)
+                    .fontWeight(.bold)
+                
+                Text("""
+                     Good job completing all four exercises!
+                     Remember tomorrow's another day.
+                     So eat well and get some rest.
+                     """)
+                .foregroundColor(.gray)
+                .multilineTextAlignment(.center)
+            }
+            
+            VStack {
+                Spacer()
+                Button("Continue") { }
+                    .padding()
+            }
+        }
+    }
+}
+
+#Preview {
+    SuccessView()
+}
--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -26,11 +26,20 @@ struct WelcomeView: View {
                             .font(.headline)
                     }
                     Image("step-up")
-                        .resizable()
-                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
-                        .frame(width: 240.0, height: 240.0)
+                        .resizedToFill(width: 240.0, height: 240.0)
                         .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
                 }
+                
+                Button(action: { }) {
+                    Text("Get Started")
+                    Image(systemName: "arrow.right.circle")
+                }
+                .font(.title2)
+                .padding()
+                .background(
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color.gray, lineWidth: 2)
+                )
             }
         }
     }
