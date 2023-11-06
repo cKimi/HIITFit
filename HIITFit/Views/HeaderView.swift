15fc4ee89b6a928de2e7c8384ce4f1f1bb0bb3c0
Carlos Kimura <ckimura@gmail.com>
2023-11-06 16:09:12 -0300

    header view refactor and welcome view created

--- HIITFit/Views/HeaderView.swift
+++ HIITFit/Views/HeaderView.swift
@@ -9,13 +9,14 @@ import SwiftUI
 
 struct HeaderView: View {
     
-    let exerciseName: String
+    let titleText: String
     
     var body: some View {
         VStack {
-            Text(exerciseName)
+            Text(titleText)
                 .font(.largeTitle)
             HStack {
+                Image(systemName: "hand.wave")
                 Image(systemName: "1.circle")
                 Image(systemName: "2.circle")
                 Image(systemName: "3.circle")
@@ -28,6 +29,6 @@ struct HeaderView: View {
 
 
 #Preview {
-    HeaderView(exerciseName: "Squat")
+    HeaderView(titleText: "Squat")
         .previewLayout(/*@START_MENU_TOKEN@*/.sizeThatFits/*@END_MENU_TOKEN@*/)
 }
--- HIITFit/Views/WelcomeView.swift
+++ HIITFit/Views/WelcomeView.swift
@@ -9,7 +9,30 @@ import SwiftUI
 
 struct WelcomeView: View {
     var body: some View {
-        Text("Hello, World!")
+        ZStack {
+            VStack {
+                HeaderView(titleText: "Welcome")
+                Spacer()
+                Button("History") { }
+                    .padding(.bottom)
+            }
+            
+            VStack {
+                HStack(alignment: .bottom) {
+                    VStack(alignment: .leading) {
+                        Text("Get fit")
+                            .font(.largeTitle)
+                        Text("with high intensity interval training")
+                            .font(.headline)
+                    }
+                    Image("step-up")
+                        .resizable()
+                        .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                        .frame(width: 240.0, height: 240.0)
+                        .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                }
+            }
+        }
     }
 }
 
